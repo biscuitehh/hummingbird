717f441146b07436630abf6876ed6de7e7561105
Michael Thomas <michael.thomas@willowtreeapps.com>
2016-04-11 11:46:11 -0400

    Use open-swift S4

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
 	name: "Hummingbird",
     dependencies: [
         .Package(url: "https://github.com/ketzusaka/Strand", majorVersion: 1, minor: 1),
-	.Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 2)
+	.Package(url: "https://github.com/open-swift/C7.git", majorVersion: 2)
     ]
 )
 
